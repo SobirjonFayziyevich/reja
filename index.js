ef11e7287634a8701239a5be5e00d0a3b052fb2b
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-17 20:57:06 +0900

    BRR: Initial setup

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-// Model packagelar 3 xil:
-// (1) CORE package:
+function fruit (str1, str2){
+  // Harflarni ketma- krtlikda joylashtirib olamiz.
 
-setTimeout(function () {
-    console.log("ishga tushdi");
-    }, 5000); //5000 degani 5 sekund;
-    
-    let number = 0;
-    setInterval(function () { //har bir sekundga kodlar harakatini bizga ishga tushirib beraveradi.
-      console.log("hisob", number);
-      number++;  // look qilganda qiymatini bir baravariga oshirib borish.
-    }, 1000);  //buyerdagi son 100 yoki 0 bulsa sonning uzgarish tezligi ortib boraveradi.
-    
-    const { clear } = require('console');
-    //fs - fileSystem;
-    
-    const fs = require('fs');
-    const {networkInterface } = require('os');
-    const data = fs.readFileSync('./input.txt', 'utf8');
-    console.log(data);
-    
-    console.log("*****************");
-    //yuqoridagi fail systemga uzgartirish kiritamiz.
-    
-    fs.writeFileSync('./input.txt', `${data} \n\t\t by Sobirjon`);
-    const new_data = fs.readFileSync('./input.txt', 'utf8');
-    console.log(new_data);
-    
+  const new_fruit1 = str1.sort('').join('');
+  const new_fruit2 = str2.sort('').join('');
+  return new_fruit = fruit; 
+}
+// 
+const string1 = "olma";
+const string2 = "olcha";
+const natija = friut(string1, string2);
+console.log(natija) 
