1e1213daf1afaa449fb9d87b5faf590ab48e09b6
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-18 13:39:44 +0900

    EXTERNAL Packages

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-function fruit (str1, str2) {
-  // ikkila qatorni harf qatoriga ayirib aifbbo tartibida saqlaymiz.
+// Modellar;
+// CORE: setTimeout , setInterval, functionsystem (fs),
+
+//EXTERNAL model: moment, inquirer, requirer, validator, uuid,
+
+const moment = require("moment");
+const time = moment().format("YYYY-MM-DD");  //format ichiga hech narsa yozilmasa (bydefault) buladi.
+console.log(time);
+
+setInterval(() => {
+  const time = moment().format();
+  console.log(`hozirgi vaqt: ${time}`);
+}, 5000);
+
+const inquirer = require("inquirer");
+inquirer
+.prompt([{ type: "input", name: "raqam", message: "raqamni kiriting? " }])
+.then((answer) => {
+  console.log("men kiritgan raqam qiymati: ", answer.raqam);
+})
+.catch((err) => console.log(err));
+
+//validator;
+const validator = require("validator");
+//const test = validator.isEmail("bas@ket.com"); // email bulmasa false qiymat beradi.
+
+//const test = validator.isInt("123"); //string kurinishidagi son  bulmasa qiymat false bulaveradi.
+const test = validator.isIP("121.175.70.224"); // agar IP da turt juftlik raqamlar bulmsa false buladi.
+console.log("test:", test);
+
+// uuid:
+const { v4: uuid4 } = require("uuid");
+const random = uuid4();      //random string hosil qiladi.
+console.log("random:", random); 
+
+const chalk = require("chalk");
+const log = console.log;
+
+log(chalk.blue(" Hello ") + random + chalk.red(" ! "))
+
+log(chalk.blue(`uuid creates ${random}`)); //uuidlarim kukrangga kirdi.
+log(chalk.red(`uuid creates ${random}`)); // uuidni qizil rangga uzgartir dedim.
+ 
 
 
-  const new_fruit1 = str1.split('').sort().join('');
-  const new_fruit2 = str2.split('').sort().join('');
-
-  //harflar qatorini solishtiramiz.
-  return new_fruit1 === new_fruit2; 
-}
-const string1 = "hayr";
-const string2 = "ryah";
-const result = fruit(string1, string2);
-console.log(result);
 
 
 
+//FILE
