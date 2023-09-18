93329874f40d8be15db0dda6450db8ac4dba50c2
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-19 00:55:21 +0900

    TASK-C

---
+++ train.js
@@ -0,0 +1,15 @@
+function fruit (str1, str2) {
+    // ikkila qatorni harf qatoriga ayirib aifbbo tartibida saqlaymiz.
+  
+  
+    const new_fruit1 = str1.split('').sort().join('');
+    const new_fruit2 = str2.split('').sort().join('');
+  
+    //harflar qatorini solishtiramiz.
+    return new_fruit1 === new_fruit2; 
+  }
+  const string1 = "hayr";
+  const string2 = "ryah";
+  const result = fruit(string1, string2);
+  console.log(result);
+  
