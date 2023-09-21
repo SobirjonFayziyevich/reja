d774d168cc6aad9d351ee7f21a3667e9888737fa
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-21 20:52:15 +0900

    TASK-D

---
+++ store.js
@@ -0,0 +1,39 @@
+
+
+
+const moment = require("moment");
+
+function showMeTime() {
+    console.log(`ayni vaqt bizda  ${moment().format("DD MM YYYY HH:mm")} ni ko'rsatyabdi`)};
+
+
+class foodStore {
+
+    constructor(bread, coffee, tea) {
+        this.bread = bread;
+        this.coffee = coffee;
+        this.tea = tea;
+    }
+  
+  sell(bread, coffee, tea) {
+    this.bread -= bread; this.coffee -= coffee; this.tea -= tea;
+    console.log(`hozirda sizning foodStore ${bread} ta bread ${coffee} ta coffee va ${tea} ta tea sotildi`);
+    showMeTime();
+  }
+  
+  
+  qoshish(bread, coffee, tea) {
+    this.bread += bread; this.coffee += coffee; this.tea += tea;
+    console.log(`hozirda sizning foodStoreda ${bread} ta bread  ${coffee} ta coffee va ${tea} ta tea qoshildi`);
+    showMeTime();
+  }
+  
+  qoldiq() {
+    console.log(`Bizning foodStoreda  ${this.bread} dona bread, ${this.coffee} dona coffee, va ${this.tea} dona tea qoldi.`);
+    console.log("Yana kutub qolamiz!!");
+    showMeTime();
+  }
+  
+  }
+  
+  module.exports = foodStore;
--- train.js
+++ train.js
@@ -1,36 +1,17 @@
-TASK - D
-
-class Shop {
-  
-  constructor(bread, coffee, tea) {
-     this.bread = bread;
-     this.coffee = coffee;
-     this.tea = tea;
-  } 
-
-  sotish() {
-     console.log(`Salom bugun 12:30gacha ${this.bread}dona non, ${this.coffee} kofe va ${this.tea}dona choy sotildi.`);
-  }
-  
-  
-  
-  qabul() {
-    console.log(`Bugun qabul qilingan mahsulotlar orasida ${this.coffee}dona kofening miqdori kamroq.`);
-  }
-    
-  
- qoldiq() {
-   console.log('Bugun kechki 6:00dan keyin choy va kofedan dona qoldi.'); 
-  }
-}  
-  
- const product = new Shop(5, 8, 4);
- const oldProduct = new Shop(2, 3, 1);
- const newProduct = new Shop ();
+// //TASK - D
 
  
-product.sotish();
-oldProduct.qabul();
-newProduct.qoldiq();
-  
-  
+
+
+
+const FoodStore = require("./store");
+console.log('================');
+
+const MyfoodStore = new FoodStore(20,15,10);
+
+MyfoodStore.sell(2,2,2);
+console.log('================');
+ MyfoodStore.qoshish(10,2,4);
+ console.log('================');
+ MyfoodStore.qoldiq();
+ console.log('================');
