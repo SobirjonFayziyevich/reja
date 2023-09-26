48999511c9b19e15753be698aead43d2cf3108f8
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-26 16:56:30 +0900

    feat "done Callback functions"

--- server.js
+++ server.js
@@ -1,40 +1,73 @@
-//Backend server qurish: va node.js da expressdan foydalandik.
 
-console.log("Web Serverni boshlash");
+console.log("web serverni boshladik");
 const express = require("express");
-const app = express();  //app orqali expressning instans yasadik. yani app objectini bizga yuboradi.
+const res = require("express/lib/response");
+const app = express();
 const http = require("http");
+const fs = require("fs");
 
-//1: KIRISH KODLAR
-//malumotlarga kerakli kodlarni yozamiz.
-app.use(express.static("public"));              //app harqanday browserdan kirb keladigan malumotlar uchun ochiq degani. yani faqat puplic folderimni kuradi deani.
-app.use(express.json());                       //  kirib kelayotgan json formatni objectga ugirib beradi
-app.use(express.urlencoded({extended: true})); //HTML traditional reques form post qilgan malumotni express qabul qilib oladi. 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err)
+    } else {
+        user = JSON.parse(data)
+    }
+});
+//1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-//2: Session code;
-//3: Views code.... Backendda Fronted yasaymiz;
-app.set("views", "views"); 
-app.set("view engine", "ejs");         //views folderni ichidan malumot olib uqiydi. agar folder nomini uzgartirsak (views) folderni ham nomini uzg
+// 2: Session code
+// 3: Views code
 
+app.set("views","views");
+app.set("view engine",  "ejs",);
 
-//4 Routing code  roterlarga muljallanga ;
+// 4 Routing code
 
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success" });
+   //TODO: code with bd here
 });
 
-app.get("/", function (req, res) {
-    res.render("harid");
+app.get("/author", (req, res) => {
+    res.render("author", {user: user})
 });
 
+app.get("/", function (req,res)  {
+     res.render("harid");
+});
 
-//http bizning CORE MODEL
-const server = http.createServer(app); // bu server orqali yasab olgan EXPRESS app ozgaruvchimizni pass qilishimiz kerak.
+const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`the server is running successfully on port: ${PORT}`)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- train.js
+++ train.js
@@ -1,17 +1,37 @@
-// //TASK - D
-
- 
 
 
+console.log("Jack Ma maslahatlar");
+const list = [
+ 'yaxshi talaba boling', //0-20
+ 'togri boshiq tanlang va koproq hato qiling', // 20-30
+ 'oz ishingizni boshlang', // 30-40
+ 'siz kuchli bolgan narsalarni qiling', //40-50
+ 'yoshlarga investitsiya qiling', // 50-60
+ 'endi dam oling, foydasi yoq'
+];
 
-const FoodStore = require("./store");
-console.log('================');
 
-const MyfoodStore = new FoodStore(20,15,10);
+function maslahatbering (a, callback) {
+ if ( typeof a !== "number") callback("insert a number", null);
+ else if (a <= 20) callback(null, list[0]);
+ else if (a > 20 && a <= 30) callback(null, list[1]);
+ else if (a > 30 && a <= 40) callback(null, list[2]);
+ else if (a > 40 && a <= 50) callback(null, list[3]);
+ else if (a > 50 && a <= 60) callback(null, list[4]);
+ else {
+  setTimeout(function () {
+   callback(null, list[5]);
+  }, 5000);
+ }
+}
 
-MyfoodStore.sell(2,2,2);
-console.log('================');
- MyfoodStore.qoshish(10,2,4);
- console.log('================');
- MyfoodStore.qoldiq();
- console.log('================');
+
+console.log("passed here 0")
+maslahatbering(25, (err, data) => {
+ if(err) console.log("ERROR:", err)
+ else {
+  console.log("javob:", data);
+ }
+})
+
+console.log("passed here 1");
