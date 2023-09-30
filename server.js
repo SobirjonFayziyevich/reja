729c1f9396f605bea48067cbf3afa9222269c61e
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-09-30 17:23:47 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,77 +1,34 @@
 
-console.log("web serverni boshladik");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+// const app = require("./app");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err)
-    } else {
-        user = JSON.parse(data)
-    }
+let db;
+const connectionString = "mongodb+srv://pirmatovsobir23:KnSHlq9VR0dnUTzc@cluster0.kscois6.mongodb.net/Reja";
+
+mongodb.connect(
+ connectionString,
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+    (err, client) => {
+   if (err) console.log("ERROR on connection MongoDB");
+   else {
+       console.log("MongoDB connection succeed");
+       // console.log(client);
+       module.exports = client;
+
+       const app = require("./app");
+       const server = http.createServer(app);
+       let PORT = 3000;
+       server.listen(PORT, function () {
+           console.log(`the server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+           );
+       });
+   }
+
 });
-//1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session code
-// 3: Views code
-
-app.set("views","views");
-app.set("view engine",  "ejs",);
-
-// 4 Routing code
-
-app.post("/create-item", (req, res) => {
-   //TODO: code with bd here
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user})
-});
-
-app.get("/", function (req,res)  {
-     res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`the server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-    );
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
